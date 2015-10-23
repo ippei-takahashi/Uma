71a5c556cb73f6283c0e08652b520d9fed2ff224
高橋一平 <ippnin@gmail.com>
2015-10-23 20:08:39 +0900

    pred

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -56,10 +56,10 @@ object Main {
         val stdAndOddsThirdScore = (m - stdAndOddsThird(5)) * 10 / s + 50
 
         raceCount += 1
-        if (stdAndOddsThirdScore > 65 && stdAndOddsSecondScore < 60 && oddsScore < 60) {
+        if (stdAndOddsSecondScore > 65 && oddsScore < 60) {
           over60Count += 1
-          if (stdAndOddsThird(2) == 1.0) {
-            oddsCount += stdAndOddsThird(3)
+          if (stdAndOddsSecond(2) == 1.0) {
+            oddsCount += stdAndOddsSecond(3)
             over60WinCount += 1
           } else {
             over60LoseCount += 1
