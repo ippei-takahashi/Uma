763edbd98a717f7acc37de4f1fabcafb3483c27d
高橋一平 <ippnin@gmail.com>
2015-11-17 13:31:33 +0900

    over 70

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -56,7 +56,7 @@ object Main {
         val stdAndOddsThirdScore = (m - stdAndOddsThird(8)) * 10 / s + 50
 
         raceCount += 1
-        if (stdScore > 65 && oddsScore < 60 && s != 0) {
+        if (stdScore > 70 && oddsScore > 62 && s != 0) {
           over60Count += 1
           if (stdHead(2) == 1.0) {
             oddsCount += stdHead(3)
